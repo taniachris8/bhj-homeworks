c63a3b9800af8855c6f04be79c2bf87ab8a9649a
taniachris8 <dove6464dove@gmail.com>
2025-07-10 12:13:57 +0300

    dropdown done

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const dropdownValue = dropdown.querySelector(".dropdown__value");
+  const dropdownList = dropdown.querySelector(".dropdown__list");
+
+  dropdownValue.addEventListener("click", function () {
+    dropdownList.classList.toggle("dropdown__list_active");
+  });
+
+  const dropdownLinks = dropdown.querySelectorAll(".dropdown__link");
+
+  for (let i = 0; i < dropdownLinks.length; i++) {
+    dropdownLinks[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      dropdownValue.textContent = dropdownLinks[i].textContent;
+    });
+  }
+});
