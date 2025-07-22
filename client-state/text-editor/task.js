1a66b32461b06c4799f1c0d87a214a8902abea0c
taniachris8 <dove6464dove@gmail.com>
2025-07-22 15:12:07 +0300

    text-editor done

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,28 @@
+const editor = document.querySelector("#editor");
+const card = document.querySelector(".card");
+
+editor.value = localStorage.getItem("value");
+
+editor.addEventListener("input", (event) => {
+  localStorage.setItem("value", event.target.value);
+});
+
+const clearBtn = document.createElement("button");
+clearBtn.className = "clear-btn";
+clearBtn.style.cssText = `
+  background-color: red;
+  color: white;
+  width: 120px;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+`;
+clearBtn.textContent = "Очистить";
+card.append(clearBtn);
+
+const btn = document.querySelector(".clear-btn");
+
+btn.addEventListener("click", () => {
+  localStorage.clear();
+  location.reload();
+});
