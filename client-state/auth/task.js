c757b304ecab1e2b4e5982e09976647875d7685b
taniachris8 <dove6464dove@gmail.com>
2025-07-23 12:10:20 +0300

    added try/catch and logout btn

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,12 +3,42 @@ const welcomeDiv = document.querySelector(".welcome");
 const spanUserId = document.querySelector("#user_id");
 const form = document.querySelector("#signin__form");
 
-const storedId = JSON.parse(localStorage.getItem("user_id"));
+function createLogoutButton() {
+  const logoutBtn = document.createElement("button");
+  logoutBtn.className = "logout-btn";
+  logoutBtn.style.cssText = `
+    background-color: green;
+    color: white;
+    width: 120px;
+    height: 40px;
+    border: none;
+    cursor: pointer;
+    margin-left: 20px;
+    `;
+  logoutBtn.textContent = "Выйти";
+  welcomeDiv.append(logoutBtn);
+
+  const btn = document.querySelector(".logout-btn");
+
+  btn.addEventListener("click", () => {
+    localStorage.clear();
+    location.reload();
+  });
+}
+
+let storedId = null;
+
+try {
+  storedId = JSON.parse(localStorage.getItem("user_id"));
+} catch (error) {
+  console.error("Ошибка парсинга JSON:", error);
+}
 
 if (storedId) {
   signin.classList.remove("signin_active");
   welcomeDiv.classList.add("welcome_active");
   spanUserId.textContent = storedId;
+  createLogoutButton();
 } else {
   signin.classList.add("signin_active");
 
@@ -19,18 +49,25 @@ if (storedId) {
 
     xhr.addEventListener("load", () => {
       if (xhr.status >= 200 && xhr.status < 400) {
-        const data = JSON.parse(xhr.responseText);
+        try {
+          const data = JSON.parse(xhr.responseText);
 
-        if (data.success) {
-          localStorage.setItem("user_id", JSON.stringify(data.user_id));
-          signin.classList.remove("signin_active");
-          welcomeDiv.classList.add("welcome_active");
-          spanUserId.textContent = data.user_id;
-        } else {
-          alert("Неверный логин или пароль");
+          if (data.success) {
+            localStorage.setItem("user_id", JSON.stringify(data.user_id));
+            signin.classList.remove("signin_active");
+            welcomeDiv.classList.add("welcome_active");
+            spanUserId.textContent = data.user_id;
+            createLogoutButton();
+          } else {
+            alert("Неверный логин или пароль");
+          }
+        } catch (error) {
+          console.error("Ошибка парсинга JSON:", error);
+          alert("Ошибка сервера, попробуйте ещё раз позже");
         }
       } else {
-        alert("Ошибка сервера ");
+        console.error("Ошибка HTTP запроса");
+        alert("Ошибка сервера");
       }
     });
 
@@ -38,5 +75,7 @@ if (storedId) {
 
     const formData = new FormData(form);
     xhr.send(formData);
+
+    form.reset();
   });
 }
